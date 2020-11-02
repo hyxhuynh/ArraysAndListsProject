501d540feb1e624fbeb023dd7019d51a5beced74
Hy Huynh <hyxhuynh@gmail.com>
2020-11-01 23:31:33 -0800

    added solutions

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArraysAndLists
 {
@@ -9,11 +10,21 @@ namespace ArraysAndLists
             //TODO:
 
             // Create an int Array and populate numbers 1-10
-
+            var arr = new int[10];
+            for (int i = 1; i <= 10; i++)
+            {
+                arr[i - 1] = i;
+            }
+            foreach (var num in arr)
+            {
+                Console.WriteLine(num);
+            }
             /* Create two Lists of type int.
              * Name one List "evens"
              * Name the other List "odds"
              */
+            var evens = new List<int>();
+            var odds = new List<int>();
 
             /* Using either a foreach or for loop,
              * nest an if statement to check to see
@@ -21,12 +32,37 @@ namespace ArraysAndLists
              * Then add those numbers to either the evens List
              * or the odds List
              */
+            for (int i = 0; i < arr.Length; i++)        
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    evens.Add(arr[i]);
+                } 
+                else
+                {
+                    odds.Add(arr[i]);
+                }
+            }
 
             /* Now using foeach or for loops,
-             * display each List of even and odd numbers
-             *
-             * Try to be creative in your display
-             */
+            * display each List of even and odd numbers
+            *
+            * Try to be creative in your display
+            */
+            Console.WriteLine("===========================");
+            Console.WriteLine("Odd numbers are: ");
+            foreach (var num in odds)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.WriteLine("===========================");
+            Console.WriteLine("Even numbers are: ");
+            foreach (var num in evens)
+            {
+                Console.WriteLine(num);
+            }
+   
         }
     }
 }
